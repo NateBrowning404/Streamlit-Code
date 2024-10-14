0de3ea6da4a3a39f4db8aea613b76b83f2fac9c3
NateBrowning404 <njbrowni@berkeley.edu>
2024-10-14 19:42:13 -0400

    Main new

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",
-        options = ["Home","Background","Contact"],
-        icons= ["house","book","envelope"],
-        menu_icon="cast",
-        #default_index=0,
-        orientation="horizontal",
-    )
+
+selected = option_menu(
+    menu_title="Main Menu",
+    options = ["Home","Background","Contact"],
+    icons= ["house","book","envelope"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+)
 
 if selected == "Home":
     st.title(f"You have selected {selected}")
