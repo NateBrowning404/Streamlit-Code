485a5d2a412ab55f5add88b44f022b3b33ae8bdb
NateBrowning404 <njbrowni@berkeley.edu>
2024-10-15 00:35:54 -0400

    Functional nav?

--- Main.py
+++ Main.py
@@ -1,15 +1,40 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import Home, Background,Contact
 
 
-selected = option_menu(
-    menu_title = None,
-    options = ["Home","Background","Contact"],
-    icons= ["house","book","envelope"],
-    #menu_icon="cast",
-    default_index=0,
-    orientation="horizontal",
+class MultiApp: 
+
+    def __init__(self):
+        self.apps = []
+    def add_app(self,title,function):
+        self.apps.append({
+            "title": title,
+            "function": function
+        })
+
+    def run():
+        selected = option_menu(
+            menu_title = None,
+            options = ["Home","Background","Contact"],
+            icons= ["house","book","envelope"],
+            #menu_icon="cast",
+            default_index=1,
+            styles={
+                "container": {"background-color":'blue'},
+                "icon": {"color": "white"}
+                "nav-link": {"color": "white"}
+                "nav-link-selected": {"background-color": "yellow"}
+            }
+            orientation="horizontal",
+
+            if app == 'Home':
+                Home.app()
+            if app == 'Background':
+                Background.app()
+            if app == 'Contact':
+                Contact.app()
+
+    run()
 )
 
-st.sidebar.page_link("Home.py", label="Home")
-st.sidebar.page_link("Background.py", label="Background")
--- Pages/Background.py
+++ Pages/Background.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.write("background page check")
+def app():
+    st.write("background page check")
