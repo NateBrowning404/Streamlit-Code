bcf2175b13677341783e95efaa11cfd74ee0ac7c
NateBrowning404 <njbrowni@berkeley.edu>
2024-10-15 00:47:16 -0400

    m

--- Main.py
+++ Main.py
@@ -1,16 +1,13 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import Home, Background,Contact
 
 
-class MultiApp: 
-
-    selected = option_menu(
-        menu_title = None,
-        options = ["Home","Background","Contact"],
-        icons= ["house","book","envelope"],
-        #menu_icon="cast",
-        default_index=0,
-        orientation="horizontal",
+selected = option_menu(
+    menu_title = None,
+    options = ["Home","Background","Contact"],
+    icons= ["house","book","envelope"],
+    #menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
 )
 
