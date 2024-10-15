05f8c362fc74390db7aa2f3a62457bac5258e17c
NateBrowning404 <njbrowni@berkeley.edu>
2024-10-15 00:07:47 -0400

    Main new top bar

--- Main.py
+++ Main.py
@@ -3,20 +3,14 @@ from streamlit_option_menu import option_menu
 
 
 selected = option_menu(
-    menu_title="Main Menu",
+    #menu_title="Main Menu",
     options = ["Home","Background","Contact"],
     icons= ["house","book","envelope"],
-    menu_icon="cast",
+    #menu_icon="cast",
     default_index=0,
     orientation="horizontal",
 )
 
-if selected == "Home":
-    st.title(f"You have selected {selected}")
-if selected == "Background":
-    st.title(f"You have selected {selected}")
-if selected == "Contact":
-    st.title(f"You have selected {selected}")
 
 st.title("Test Site")
 st.write("Please work, please")
