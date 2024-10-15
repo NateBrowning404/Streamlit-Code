9b3ca439ac228fbed9d14593be63986ac878e882
NateBrowning404 <njbrowni@berkeley.edu>
2024-10-15 01:06:35 -0400

    nu

--- Main.py
+++ Main.py
@@ -5,16 +5,15 @@ from streamlit_option_menu import option_menu
 selected = option_menu(
     menu_title = None,
     options = ["Home","Background","Contact"],
-    functions = [lambda: run_home(), lambda: run_background(), lambda:run_contact()]
     icons= ["house","book","envelope"],
     #menu_icon="cast",
     default_index=0,
     orientation="horizontal",
 )
 
-def run_home():
-    st.write("Running Home.py")
-def run_background():
-    st.write("Running Background.py")
-def run_contact():
-    st.write("Running Contact.py")
+if selected == "Home":
+    st.switch_page("Home.py")
+if selected == "Background":
+    st.switch_page("Background.py")
+if selected == "Contact":
+    st.switch_page("Contac.py")
