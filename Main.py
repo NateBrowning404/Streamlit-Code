0e47009657d94996e5fd76a7c763e89e1e9bcaf0
NateBrowning404 <njbrowni@berkeley.edu>
2024-10-15 00:43:56 -0400

    Another main update

--- Main.py
+++ Main.py
@@ -5,30 +5,12 @@ import Home, Background,Contact
 
 class MultiApp: 
 
-    def __init__(self):
-        self.apps = []
-    def add_app(self,title,function):
-        self.apps.append({
-            "title": title,
-            "function": function
-        })
-
-    def run():
-        app = option_menu(
-            menu_title = None,
-            options = ["Home","Background","Contact"],
-            icons= ["house","book","envelope"],
-            #menu_icon="cast",
-            default_index=1,
-            orientation="horizontal",
-            )
-
-        if app == 'Home':
-            Home.app()
-        if app == 'Background':
-            Background.app()
-        if app == 'Contact':
-            Contact.app()
-
-    run()
+    selected = option_menu(
+        menu_title = None,
+        options = ["Home","Background","Contact"],
+        icons= ["house","book","envelope"],
+        #menu_icon="cast",
+        default_index=0,
+    orientation="horizontal",
+)
 
